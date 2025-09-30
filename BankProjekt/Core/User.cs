c5664c2544fbe4d24d668d499e6bffe75714610d
Vivienne Bengtsson <vivienne114bengtsson@gmail.com>
2025-09-30 16:47:19 +0200

    Created an User class.

---
+++ BankProjekt/Core/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.Core
+{
+    internal class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Account> Accounts { get; set; }
+
+        public User(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
