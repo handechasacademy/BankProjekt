59fa75c9d79b2fb554d278e02663ef2a53b24fd7
BwunLevain <vivienne114bengtsson@gmail.com>
2025-10-27 14:12:26 +0100

    Add LoginUI and LoginServices classes
    Introduced two new internal classes: `LoginUI` in the `BankProjekt.ConsoleUI` namespace and `LoginServices` in the `BankProjekt.Core.Services` namespace. Both classes are currently empty and serve as a foundation for future development.

---
+++ BankProjekt/ConsoleUI/LoginUI.cs
@@ -0,0 +1,15 @@
+﻿using BankProjekt.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace BankProjekt.ConsoleUI
+{
+    internal class LoginUI
+    {
+    }
+}
